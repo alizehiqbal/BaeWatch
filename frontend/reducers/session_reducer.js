b29fe99b954cba83e1144eff6ac3437459873684
alizehiqbal <aiqbal.14@gmail.com>
2018-04-04 15:54:32 -0700

    Initialize rails backend

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+const _nullUser = Object.freeze({
+  currentUser: null
+});
+const sessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return Object.assign({}, current_user);
+    default:
+      return state;
+  }
+};
