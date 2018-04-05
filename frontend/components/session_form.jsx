bb6e41d4538ea6b989fc3430fe3bb12f775ed226
alizehiqbal <aiqbal.14@gmail.com>
2018-04-05 15:26:41 -0700

    Add a sign up form container

---
+++ frontend/components/session_form.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  update(field) {
+    return e =>
+      this.setState({
+        [field]: e.currentTarget.value
+      });
+  }
+
+  handleSubmit(e) {
+    e.
+  }
+}
