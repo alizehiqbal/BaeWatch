50ddc6fed00cfc7f8dd4cfe57e80f7548a8c4d27
alizehiqbal <aiqbal.14@gmail.com>
2018-04-05 11:54:37 -0700

    Complete rootReducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from "redux";
+
+import errors from "./errors_reducer";
+import session from "./session_reducer";
+
+const rootReducer = combineReducers({
+  errors,
+  session
+});
+
+export default rootReducer;
