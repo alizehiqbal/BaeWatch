0d294e18a28a9801b62814c7fd4ea2233565324a
alizehiqbal <aiqbal.14@gmail.com>
2018-04-08 17:03:55 -0700

    alter routing

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Route, Link, HashRouter, Redirect, Switch } from "react-router-dom";
 import SignupFormContainer from "./signup_form_container";
 import LoginFormContainer from "./login_form_container";
-
+import UserIndex from "./users/user_index";
 const App = () => (
   <div>
     <header>
       <h1>Bae Watch</h1>
     </header>
-
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <Switch>
+      <Route path="/login" component={LoginFormContainer} />
+      <Route path="/signup" component={SignupFormContainer} />
+      <Route path="/" component={Splash} />
+      <Route path="/dashboard" component={UserIndex} />
+    </Switch>
   </div>
 );
 
