d115175497984cd1cbc25e70a7f81c06808f9f28
alizehiqbal <aiqbal.14@gmail.com>
2018-04-10 09:33:21 -0700

    Remove react-slick and react-dropdown due to dependencies

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import NavBarContainer from "./navbar_container";
-import SearchingFor from "./searching_for";
-import SimpleSlider from "./backroundslides";
+// import SearchingFor from "./searching_for";
+// import SimpleSlider from "./backroundslides";
+// <SimpleSlider />
+// I'm a <SearchingFor /> looking to pair program with a <SearchingFor />
 
 const Splash = () => (
   <section>
     <NavBarContainer />
-    <SimpleSlider />
-    <div>
-      I'm a <SearchingFor /> looking to pair program with a <SearchingFor />
-    </div>
+    <div>I'm a Rubyist looking to pair program with a Pythonista.</div>
   </section>
 );
 
